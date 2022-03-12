a6315e9345e5821e4ac7693297dcabae99eace5d
Cinimajig <jacob.mogensen5@gmail.com>
2022-03-12 12:28:12 +0100

    docstring

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,19 +1,30 @@
 use ::std::mem;
 
+/// A buffer type, that is is supposed to change size on use with a C-function.
 #[repr(transparent)]
 pub struct DynBuffer {
     inner: Vec<u8>,
 }
 
 impl DynBuffer {
+    /// Creates a new empty [`DynBuffer`].
     pub fn new() -> Self {
         Self {
             inner: Vec::new()
         }
     }
 
-    pub fn len(&self) -> usize {
-        self.inner.len()
+    /// Creates a new [`DynBuffer`] with a given capacity. This helps if it needs to be 
+    /// differernt sized multiple times.
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            inner: Vec::with_capacity(capacity)
+        }
+    }
+
+    /// Returns the number of elements of the buffer as T.
+    pub fn len<T>(&self) -> usize {
+        self.inner.len() / mem::size_of::<T>()
     }
 
     pub fn as_dyn_ptr<T>(&mut self, count: usize) -> *mut T {
